7b4ffbc1cbfdd7dd0685d812f09fa51e5b7e44da
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-02-24 20:36:13 -0300

    Adjustment to get completed challenges number from the Challenge's Context

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,15 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContexts'
+
+
 import styles from '../styles/components/CompletedChallenges.module.css'
 
 export function CompletedChallenges() {
+  const { challengeCompleted } = useContext(ChallengesContext)
   return (
     <div className={styles.completedChallengedContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengeCompleted}</span>
     </div>
   )
 }

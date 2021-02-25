e706c34a111a95a29422237a19c79229beefd118
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-02-24 22:10:19 -0300

    Adjustments realized during video recording

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContexts'
+
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
   return (
     <div className={styles.profileContainer} >
       <img src="https://github.com/iagoocesaar.png" alt="IagooCesaar"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Iago César F. Nogueira</strong>
         <p>
           <img src="icons/level.svg" alt=""/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
